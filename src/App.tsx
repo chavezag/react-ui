68213b213b1a0dcde335c45a0c13c297e57226d2
chavezag <angelchavez.325.ag@gmail.com>
2022-04-16 12:44:02 -0600

    feat: :sparkles: Formulario de login y validaciones
    Creacion de formulario para el inicio de sesion de los usuarios on sus respectivas validaciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Container from "@mui/material/Container";
-import { Button, Typography } from "@mui/material";
 import { NavBar } from "./modules/ui/components/NavBar";
-import "./styles/index.scss";
 import { SideBar } from "./modules/ui/components/SideBar";
 import { MainRouter } from "./routers/MainRouter";
 import { BackdropFarinter } from "./modules/ui/components/BackdropFarinter";
+
+import "./styles/index.scss";
 
 function App() {
   const [loading, setloading] = useState(true);
